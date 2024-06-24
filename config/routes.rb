b42e8ba245b44651c700eb99b2a48f91e0373e6e
Andrew Nesbitt <andrewnez@gmail.com>
2024-06-24 11:24:10 +0100

    Fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :jobs do
+      resources :jobs, only: [:create, :show] do
         collection do
           get :sbom_formats
         end
