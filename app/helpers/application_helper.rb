d3210316476a89e4cd41be002bf4f646eb91c6d6
Andrew Nesbitt <andrewnez@gmail.com>
2025-02-21 17:25:37 +0000

    Add bootstrap-icons-helper gem and update application styles and meta information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def meta_title
+    [@meta_title, 'Ecosyste.ms: SBOM'].compact.join(' | ')
+  end
+
+  def meta_description
+    @meta_description || app_description
+  end
+
+  def app_name
+    "SBOM"
+  end
+
+  def app_description
+    'An open API service to parse and convert between SBOM file formats.'
+  end
 end
